0fe7e033f154720af2ea1529b32baf3045c6b4b4
Walcker Gomes <walckergomes@gmail.com>
2022-04-14 12:48:13 -0300

    Funcionando.

--- src/main/java/circle/Main.java
+++ src/main/java/circle/Main.java
@@ -1,17 +1,17 @@
 package circle;
 
-import java.util.Scanner;
+import java.util.Scanner; // Para usar a funcao scanner e pedir o raio ao usuario
 
 public class Main {
 
     public static void main(String[] args) {
 
-        var scanner = new Scanner(System.in);
+        var scanner = new Scanner(System.in); // chama a funcao scanner
         System.out.println("Entre com o raio do circulo.");
-        float radius = Float.parseFloat(scanner.next());
+        float radius = Float.parseFloat(scanner.next()); //espera o usuario digitar o raio e coloca na variaval
 
-        Circle calc = new Circle(radius);
-        calc.circumference();
-        calc.area();
+        Circle calc = new Circle(radius); //envia o raio para a classe Circle
+        calc.circumference(); //chama metodo para calcular a circunferencia
+        calc.area(); //chama metodo para calcular a area
     }
 }
